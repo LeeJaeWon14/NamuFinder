aecf75f6be94746207208c8004060d073a0a7ed0
ghkdw <ghkdwpqndtls@naver.com>
2021-04-24 17:07:37 +0900

    2021/04/24, 17:07.

--- app/src/main/java/com/example/namufinder/MainActivity.kt
+++ app/src/main/java/com/example/namufinder/MainActivity.kt
@@ -14,10 +14,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.namufinder.adapter.BookmarkRecyclerAdapter
 import com.example.namufinder.adapter.MyRecyclerAdapter
+import com.example.namufinder.adapter.RecordRecyclerAdapter
 import com.example.namufinder.databinding.ActivityMainBinding
 import com.example.namufinder.room.BookmarkDAO
 import com.example.namufinder.room.MyRoomDatabase
+import com.example.namufinder.room.RecordDAO
 import com.example.namufinder.room.RecordEntity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -83,6 +86,9 @@ class MainActivity : AppCompatActivity() {
                         }, items = croll)
                     }
                 }
+
+                topLine.visibility = View.VISIBLE
+                bottomLine.visibility = View.VISIBLE
             }
 
             //키패드 자동 다운
@@ -107,6 +113,8 @@ class MainActivity : AppCompatActivity() {
                 recView.adapter = BookmarkRecyclerAdapter(entities, clickListener = object : BookmarkRecyclerAdapter.OnItemClickListener {
                     override fun onItemClick(v: View, pos: Int, title: String) {
                         //리싸이클러뷰의 item click listener 정의
+
+                        //해당 항목으로 웹뷰 이동
                         goWebView(title.split(". ")[1])
                     }
                 }, longClickListener = object : BookmarkRecyclerAdapter.OnItemLongClickListener {
@@ -116,11 +124,9 @@ class MainActivity : AppCompatActivity() {
                             .setMessage("삭제하시겠습니까?")
                             .setPositiveButton("삭제", object : DialogInterface.OnClickListener {
                                 override fun onClick(dialog: DialogInterface?, which: Int) {
-                                    //entity get
                                     val entity = entities.get(pos)
                                     //가져온 entity로 delete 실행
                                     getBookmarkDAO().deleteBookmark(entity)
-                                    dlg.dismiss()
                                 }
                             })
                             .setNegativeButton("취소", null)
@@ -131,34 +137,39 @@ class MainActivity : AppCompatActivity() {
             dlg.show()
         }
 
-        fun selectRecord(view : View) {
-            val dlgView = View.inflate(this@MainActivity, R.layout.list_layout, null)
+        fun showRecrod(view : View) {
+            capabilityCheckRecord()
+
+            val entities = getRecordDAO().getRecord()
+            println("entities >> ${entities.toString()}")
+
+            val dlgView = View.inflate(this@MainActivity, R.layout.record_layout, null)
             val dlg = AlertDialog.Builder(this@MainActivity).create()
 
             dlg.setView(dlgView)
             dlg.window?.setBackgroundDrawableResource(R.drawable.block)
 
-            val list : ListView = dlgView.findViewById(R.id.recordList)
-            list.setOnItemClickListener { parent, view, position, id ->
-                //goWebView(parent.adapter.getItem(position).toString())
-                Toast.makeText(
-                    this@MainActivity,
-                    parent.adapter.getItem(position).toString(),
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
+            val recordRecycler = dlgView.findViewById<RecyclerView>(R.id.recordRecycler)
+            recordRecycler.layoutManager = LinearLayoutManager(this@MainActivity)
+            recordRecycler.adapter = RecordRecyclerAdapter(entities, object : RecordRecyclerAdapter.OnItemClickListener {
+                override fun onItemClick(v: View, pos: Int, title: String) {
+                    //ItemClick 구현
+                    goWebView(title)
+                }
+            }, object : RecordRecyclerAdapter.OnItemDeleteListener {
+                override fun onItemDeleteListener(v: View, pos: Int, entities: List<RecordEntity>) {
+                    //ItemDelete 구현
+                    val entity = entities.get(pos)
+                    getRecordDAO().deleteRecord(entity)
+
+                    entities.drop(pos)
+
+                    Snackbar.make(v, "삭제되었습니다", Snackbar.LENGTH_SHORT).show()
+                }
+            })
+
             dlg.show()
         }
-    }
-
-    //RecyclerView 대체
-    private fun listAdapterInit() : ArrayAdapter<String> {
-        val arrayList : ArrayList<String> = ArrayList<String>()
-        CoroutineScope(Dispatchers.IO).launch {
-
-        }
-        val adapter = ArrayAdapter<String>(this@MainActivity, android.R.layout.simple_list_item_1, arrayList)
-        return adapter
     }
 
     private fun goWebView(keyword : String) {
@@ -171,5 +182,17 @@ class MainActivity : AppCompatActivity() {
 
     fun getBookmarkDAO() : BookmarkDAO {
         return MyRoomDatabase.getInstance(this@MainActivity).getBookmarkDAO()
+    }
+
+    fun getRecordDAO() : RecordDAO {
+        return MyRoomDatabase.getInstance(this@MainActivity).getRecordDAO()
+    }
+
+    //저장된 기록이 5개 이상이면 마지막 기록 삭제
+    fun capabilityCheckRecord() {
+        val entities = getRecordDAO().getRecord()
+        if(entities.size > 50) {
+            getRecordDAO().deleteRecord(entities.get(entities.size -1))
+        }
     }
 }
---
+++ app/src/main/java/com/example/namufinder/adapter/RecordRecyclerAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.namufinder.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.namufinder.R
+import com.example.namufinder.room.BookmarkEntity
+import com.example.namufinder.room.RecordEntity
+
+class RecordRecyclerAdapter(
+    entities : List<RecordEntity>,
+    clickListener : OnItemClickListener,
+    deleteListener : OnItemDeleteListener
+) : RecyclerView.Adapter<RecordRecyclerAdapter.RecordRecyclerViewHolder>() {
+    interface OnItemClickListener {
+        fun onItemClick(v : View, pos : Int, title : String)
+    }
+    interface OnItemDeleteListener {
+        fun onItemDeleteListener(v : View, pos : Int, entities: List<RecordEntity>)
+    }
+
+    //Field 정의
+    private val entities = entities
+    private val listener = clickListener
+    private val deleteListener = deleteListener
+
+    //ViewHolder 정의
+    class RecordRecyclerViewHolder(itemView : View, listener : OnItemClickListener?, deleteListener : OnItemDeleteListener?, entities : List<RecordEntity>) : RecyclerView.ViewHolder(itemView) {
+        private val textView : TextView = itemView.findViewById(R.id.recordItem)
+        private val deleteButton : Button = itemView.findViewById(R.id.deleteButton)
+        init {
+            itemView.setOnClickListener {
+                val pos = adapterPosition
+                if(pos != RecyclerView.NO_POSITION && listener != null) {
+                    listener.onItemClick(itemView, pos, textView.text.toString())
+                }
+            }
+            deleteButton.setOnClickListener {
+                val pos = adapterPosition
+                if(pos != RecyclerView.NO_POSITION && deleteListener != null) {
+                    deleteListener.onItemDeleteListener(itemView, pos, entities)
+                }
+            }
+        }
+
+        fun setTitle(title : String) {
+            this.textView.text = title
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordRecyclerViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.record_recycle_view_item, parent, false)
+        return RecordRecyclerViewHolder(itemView, listener, deleteListener, entities)
+    }
+
+    override fun getItemCount(): Int {
+        return entities.size
+    }
+
+    override fun onBindViewHolder(holder: RecordRecyclerViewHolder, position: Int) {
+        holder.setTitle(entities.get(position).keyword)
+    }
+}
